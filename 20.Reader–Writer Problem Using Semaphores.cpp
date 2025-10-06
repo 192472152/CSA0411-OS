12019894d7e515067cc8d0440427550f4cf50a87
192472152 <shekardharanish@gmail.com>
2025-10-06 13:53:42 +0530

    Update 20.Reader–Writer Problem Using Semaphores.cpp

--- 20.Reader–Writer Problem Using Semaphores.cpp
+++ 20.Reader–Writer Problem Using Semaphores.cpp
@@ -1,9 +1,3 @@
-/* 
- ============================================================================
-    C Program to Simulate Reader-Writer Problem using Semaphores
- ============================================================================
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -85,4 +79,5 @@ int main() {
 
     return 0;
 }
+
 
